65be0ae3a2e7710b0be1cfb13ac63ac4e06c818b
Ç*** F*** <61086421+MorphaxTheDeveloper@users.noreply.github.com>
2024-03-13 16:29:36 +0300

    NetworkSubsystem2

--- src/main/java/frc/robot/subsystems/NetworkSubsystem.java
+++ src/main/java/frc/robot/subsystems/NetworkSubsystem.java
@@ -8,30 +8,36 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class NetworkSubsystem extends SubsystemBase {
 
-  String getstr;
+  double apriltag_x_value;
+  double apriltag_y_value;
 
   public NetworkTableInstance inst = NetworkTableInstance.create();
   //table
-  public NetworkTable table = inst.getTable("table");
+  public NetworkTable table = inst.getTable("vision");
 
   //subtable
-  public NetworkTable networkTable = table.getSubTable("Subtable");
+  public NetworkTable networkTable = table.getSubTable("apriltag");
 
   //entry
-  public NetworkTableEntry veri = networkTable.getEntry("veri");
+  public NetworkTableEntry apriltag_x = networkTable.getEntry("apriltag_x");
+  public NetworkTableEntry apriltag_y = networkTable.getEntry("apriltag_y");
 
   public NetworkSubsystem() {
-    inst.startClient4("10.0.0.0");
+    inst.startServer("10.76.72.2");
   }
 
-  public String return_value(){
-    return getstr;
-    }
+  public double getX(){
+    return apriltag_x_value;
+  }
+
+  public double getY(){
+      return apriltag_y_value;
+   }
 
   @Override
   public void periodic() {
-    getstr = veri.getString("0.0");
-    System.out.println(getstr);  
+    apriltag_x_value = apriltag_x.getDouble(0.0);
+    apriltag_y_value = apriltag_y.getDouble(0.0);
   }
 
 
